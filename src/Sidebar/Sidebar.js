fd47a8c9663ee20a59c2b65bf383be08849eb475
Srirm14 <sriram.venkatachalam@brandwatch.com>
2023-10-21 01:08:29 +0530

    code changes

--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -4,7 +4,7 @@ import BLOCK_DATA from "./block-data";
 
 export default function Sidebar() {
   return (
-    <div className="w-60 flex-none h-full overflow-y-auto flex flex-col items-start p-2 border-r border-gray-200">
+    <div className="w-60 flex-none h-full overflow-y-auto flex flex-col items-start p-2 border-r border-gray-300">
       {BLOCK_DATA.map((categoryData, index) => (
         <Block key={index} title={categoryData.title} data={categoryData.data} className={categoryData.blockCss} />
       ))}
